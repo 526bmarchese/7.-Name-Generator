70152c77d0d80a156cf82bf8ab7ab186be760900
526bmarchese <526bmarchese@frhsd.com>
2024-12-16 09:15:31 -0500

    Day #3: Mr. Kilgore gave us more code and put out so many fires today :) (have to do more at home)

--- script.js
+++ script.js
@@ -1,85 +1,89 @@
 //Generate Prefix
 function genPrefix(firstName) {
     if (firstName.length > 5) {
-        return `The Great`;
+        return 'The Great'
     } else {
-        return `Master`;
+        return 'Master'
     }
 }
-//Generate First Name
+//generate first name
 function genFirstName(firstName) {
-    const firstLetter = firstName.charAt(0).toLowerCase();
-    if (firstLetter === "a") {
-        return "Jeff";
-    } else if (firstLetter === "b") {
-        return "Pablo";
+    const firstLetter = firstName.charAt(0).toLowerCase()
+    if (firstLetter === 'a') {
+        return 'Jeff'
+    } else if (firstLetter === 'b') {
+        return 'Pablo'
     } else {
-        return "Julian";
+        return 'Julian'
     }
 }
 
-//Generate Middle Name
+//generate middle name
 function genMiddleName(roadType, favoriteColor) {
     if (roadType === 'road') {
-        return `${favoriteColor} ridge` //Example: Blueridge
+        return `${favoriteColor}ridge` //EX:  blueridge
     } else if (roadType === 'street') {
-        return `${favoriteColor} son` //Example: Blueson
+        return `${favoriteColor}son` //EX: blueson
     } else {
-        return `${favoriteColor} stone` //Example: Bluestone
+        return `${favoriteColor}stone` //EX: bluestone
     }
 }
 
-//Generate Last Name
+//generate Last Name
 function genLastName(lastName) {
     const lastLetter = lastName.charAt(lastName.length - 1)
-    if (lastLetter === 'a')
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    if (lastLetter === 'a') {
+        return 'Shadow'
+    } else if (lastLetter === 'e') {
+        return 'Storm'
+    } else if (lastLetter === 'i') {
+        return 'Blaze'
+    } else if (lastLetter === 'o') {
+        return 'Thorn'
+    } else if (lastLetter === 'u') {
+        return 'Frost'
+    } else {
+        return 'Moon' // Default last name for letters not matched
     }
+}
 
-//Generate Suffix
+//generate Suffix
 function genSuffix(favoriteAnimal) {
     return `of ${favoriteAnimal} clan.`
 }
 
-//MAster Name Building Function
+//Master Name Building FUnction
 function genFullName() {
-    //Get the User's Inputs from the HTML Elements
+    
+    //Get the Users Inputs from HTML Elements
     const firstName = document.getElementById('firstName').value.trim()
     const lastName = document.getElementById('lastName').value.trim()
     const roadType = document.getElementById('roadType').value
     const favoriteColor = document.getElementById('favoriteColor').value.trim()
     const favoriteAnimal = document.getElementById('favoriteAnimal').value.trim()
 
-    //Run Name Generating Functions & Store them in New Variables
+    //Run Name Generating Functions & store them in new variables
     const prefix = genPrefix(firstName)
     const newFirstName = genFirstName(firstName)
     const middleName = genMiddleName(roadType, favoriteColor)
     const newLastName = genLastName(lastName)
     const suffix = genSuffix(favoriteAnimal)
 
-    //Capitalize Name Variables when Needed
-    const capitalizedPrefix = genPrefix(prefix)
-    const capitalizedFirstName = genFirstName(newFirstName)
-    const capitalizedMiddleName = genMiddleName(middleName)
-    const capitalizedLastName = genLastName(newLastNAme)
-    //Combine all of the Name Variables in a New Name
+    //Capitalize Name Variables when needed
+    const capitalizedPrefix = capitalize(prefix)
+    const capitalizedFirstName = capitalize(newFirstName)
+    const capitalizedMiddleName = capitalize(middleName)
+    const capitalizedLastName = capitalize(newLastName)
 
-    //Display the New Name
+    //Combine all of the Name variables in a new name
+    const fullName = `${capitalizedPrefix} ${capitalizedFirstName} ${capitalizedMiddleName} ${capitalizedLastName} ${suffix}`
+    console.log(fullName)
 
+    //Display the new name
+    document.getElementById('result').textContent = fullName
 }
 
-    //Capitilization Function
-    function capitalize (input) {
-        return input.charAt (0).toUpperCAse () + input.slice (1).toLowerCase ()
-    }
+//Capitalization Function
+function capitalize(input) {
+    return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase()
+}
